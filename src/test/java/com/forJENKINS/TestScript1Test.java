29b0e3aee93af5a28d014e294fb6c58d7c8a47d5
VaishnaviKadam95 <vaishnavikadam01@gmail.com>
2023-11-30 10:09:03 +0530

    FrameWork is done

---
+++ src/test/java/com/forJENKINS/TestScript1Test.java
@@ -0,0 +1,24 @@
+package com.forJENKINS;
+
+import org.testng.annotations.Test;
+
+public class TestScript1Test {
+	@Test
+public void ts1() {
+	System.out.println("==TS1===");
+}
+	@Test
+public void ts2() {
+	System.out.println("==TS2===");
+}
+	@Test
+public void ts3() {
+	System.out.println("=TS 3===");
+	
+	
+	
+	
+	
+}
+	
+}
